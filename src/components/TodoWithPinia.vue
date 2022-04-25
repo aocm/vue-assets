8300149d0787c8aa52dded06b7a20602254957d3
aocm <aocm.dev@gmail.com>
2022-04-25 15:51:11 +0900

    add: pinia with sample
    refs
    - https://zenn.dev/sa2knight/articles/74f066b7be24c3

---
+++ src/components/TodoWithPinia.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { reactive } from 'vue'
+import { useTodoStore } from '../store/todos'
+
+const state = reactive({ newTodoLabel: '' })
+
+// useTodoStore を呼び出すだけで、グローバルストアへのアクセスが可能
+const store = useTodoStore()
+
+// ストア内の State や Getters はリアクティブオブジェクトなので、
+// リアクティブを失わずに取り出す場合は storeToRefs を用いる
+const { filteredTodos, filter } = storeToRefs(store)
+
+const toggleTodo = (id: number) => store.toggleTodo(id)
+const addTodo = () => {
+  if (state.newTodoLabel !== '') {
+    store.addTodo(state.newTodoLabel)
+    state.newTodoLabel = ''
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>TODO list with pinia</h2>
+    <div class="todo-form">
+      <input v-model="state.newTodoLabel" />
+      <button @click="addTodo">add</button>
+      <input id="all" v-model="filter" type="radio" value="all" />
+      <label for="all">すべて</label>
+      <input id="finished" v-model="filter" type="radio" value="finished" />
+      <label for="finished">完了済み</label>
+      <input id="unfinished" v-model="filter" type="radio" value="unfinished" />
+      <label for="unfinished">未完了</label>
+    </div>
+    <ul>
+      <li
+        v-for="todo in filteredTodos"
+        :key="todo.label"
+        :class="{ todo: true, finished: todo.finished }"
+        @click="toggleTodo(todo.id)"
+        v-text="todo.label"
+      />
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+  text-align: left;
+  border-style: solid;
+  border-color: black;
+}
+.todo-form {
+  text-align: center;
+}
+.todo {
+  user-select: none;
+  cursor: pointer;
+}
+.todo.finished {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import TodoComponent from '../components/TodoComponent.vue'
+import TodoWithPinia from '../components/TodoWithPinia.vue'
 </script>
 <template>
   <h1>HOME!</h1>
-  <TodoComponent />
+  <TodoWithPinia />
 </template>
